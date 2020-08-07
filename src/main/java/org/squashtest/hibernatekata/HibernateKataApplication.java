d5053f1941a38832292bc1fe4dfe564f47c36e63
Julien Thebault <jthebault@qualixo.com>
2020-08-07 16:42:31 +0200

    initial commit

---
+++ src/main/java/org/squashtest/hibernatekata/HibernateKataApplication.java
@@ -0,0 +1,13 @@
+package org.squashtest.hibernatekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/squashtest/hibernatekata/HibernateKataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.squashtest.hibernatekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
